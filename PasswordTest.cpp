29845cf76ee85669cf33380933b14e706a588429
IJNJJJCG <hardlifecansuck@gmail.com>
2026-04-13 11:38:41 -0700

    Modified: PasswordTest.cpp for practice.

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -17,6 +17,17 @@ class PasswordTest : public ::testing::Test
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+
+TEST(PasswordTest, mixed_case_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("ZZz"));
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
 }
