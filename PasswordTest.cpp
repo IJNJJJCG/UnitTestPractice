262a0344b71ca7a44ac17808923334eb3b6d963c
IJNJJJCG <hardlifecansuck@gmail.com>
2026-04-15 11:51:11 -0700

    Testing Unique Characters in Passwords and it works

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -23,11 +23,17 @@ TEST(PasswordTest, single_letter_password)
 TEST(PasswordTest, mixed_case_password)
 {
 	Password my_password;
-	ASSERT_EQ(2, my_password.count_leading_characters("ZZz"));
+	ASSERT_EQ(true, my_password.has_mixed_case("ZZz"));
 }
 
 TEST(PasswordTest, empty_password)
 {
 	Password my_password;
 	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+
+TEST(PasswordTest, unique_characters_password)
+{
+	Password my_password;
+	ASSERT_EQ(3, my_password.unique_characters("Aa1"));
 }
